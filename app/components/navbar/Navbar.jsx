1bac521613b951491b6110cb509abf13e1de99ac
ROHAN PRASAD GUPTA <90445636+RohanPrasadGupta@users.noreply.github.com>
2025-02-02 22:11:24 +0700

    updated code on product checkout

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import styles from "./navbarStyle.module.scss";
 import Link from "next/link";
 import { MdShoppingCart } from "react-icons/md";
 import { useRouter } from "next/navigation";
+import companyLogo from "./logo.jpg";
+import Image from "next/image";
 
 function Navbar() {
   const router = useRouter();
@@ -11,7 +13,15 @@ function Navbar() {
   return (
     <nav className={styles.navbarMain}>
       <div className="navbar-brand">
-        <Link href="/pages/dashboard">Logo</Link>
+        <Link href="/pages/dashboard">
+          <Image
+            className={styles.imageStyle}
+            src={companyLogo}
+            alt="Company Logo"
+            width={35}
+            height={35}
+          />
+        </Link>
       </div>
       <div className={styles["navbar-nav-items"]}>
         <div className={styles.cartButtonDiv}>
